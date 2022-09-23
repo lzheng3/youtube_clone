efaa225de7024e5eed08f1f206564ae0757553bc
lzheng3 <lekaizheng@icloud.com>
2022-09-23 00:10:09 -0400

    change one

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,23 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-
+import { Sidebar, Videos } from './';
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const Feed = () => {
-    return (
-        <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-            <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+    const [selectedCategory, setSelectedCategory] = useState("New");
 
 
-                <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-                    Copyright © 2022 JSM Media
-                </Typography>
-            </Box>
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+        .then((data) => setVideos(data.items))
+}, [selectedCategory]);
 
-            <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+        <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
 
-            </Box>
-        </Stack>
-    )
-}
+            <Sidebar
+                selectedCategory=
+                {selectedCategory}
+                setSelectedCategory={setSelectedCategory}
+            />
+            <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+                Copyright © 2022 ABC Media
+            </Typography>
+        </Box>
+
+        <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+            <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+                {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
+            </Typography>
+
+            <Videos videos={videos} />
+        </Box>
+    </Stack>
+);
+};
 
 export default Feed
